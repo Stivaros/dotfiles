03478e8d8febac0b2c93e7dc5477a72605b660a7
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2022-05-29 21:51:36 +0100

    Use unattended install flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 mv ~/.zshrc ~/.zshrc_OLD
 
